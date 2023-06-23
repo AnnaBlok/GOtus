0acc713dc29dcc1010c2b4dccf334b4aaf0ab8c9
Anna <black.doretie@yandex.ru>
2023-06-23 11:15:31 +0300

    HW is done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,37 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var regSplit = regexp.MustCompile(`[\s.,!?"':;]+`)
+
+func Top10(text string) []string {
+	wordsCount := make(map[string]int)
+	words := regSplit.Split(text, -1)
+	if len(words) == 0 {
+		return nil
+	}
+	for _, word := range words {
+		word = strings.ReplaceAll(strings.ToLower(word), "-", "")
+		if len(word) > 0 {
+			wordsCount[word] += 1
+		}
+	}
+	keys := make([]string, 0, len(wordsCount))
+	for key := range wordsCount {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	sort.SliceStable(keys, func(i, j int) bool {
+		return wordsCount[keys[i]] > wordsCount[keys[j]]
+	})
+	if len(keys) > 9 {
+		return keys[:10]
+	} else {
+		return keys
+	}
+
 }
